d64b0266e489f489be860c0dc2ebd562fd797456
jakeh5431 <jacobhenry5431@gmail.com>
2022-07-28 19:38:03 -0400

    Update 2.1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <h1>hello</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="chat" element={<Chat />} />
