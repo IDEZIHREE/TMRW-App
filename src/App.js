fd55eec500d593bdba406c2d69951e59681cb877
jakeh5431 <jacobhenry5431@gmail.com>
2022-07-28 20:05:59 -0400

    inter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>hello</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="chat" element={<Chat />} />
