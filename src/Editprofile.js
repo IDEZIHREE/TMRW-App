157b2fbb70a7ce0433bd7cf7ab34d95353d6aed2
jakeh5431 <jacobhenry5431@gmail.com>
2022-08-03 23:16:54 -0400

    update 3.7

--- src/Editprofile.js
+++ src/Editprofile.js
@@ -22,42 +22,31 @@ function Editprofile () {
             <div>
                 <form action="/action_page.php">
                     <div class="container">
-                        <h1>About Me</h1>
-                        <p>Please fill in this form to create an account</p>
-                        
-                        <label for="About Me"><b>About Me</b></label>
-                        <input type="text" placeholder="Tell us about yourself..." name="about" id="about me" required>
+                        <div className="personal_details">
+                        <label for="About Me"><b>My bio</b></label>
+                        </div>
+                        <div className="about_me_bio">
+                        <input className="about_me_input" type="text" placeholder="Write a fun introduction about yourself" name="about" id="about me" required>
                         </input>
-
-                        <label for="Hometown"><HouseIcon /><b>Hometown</b></label>
-                        <input type="text" placeholder="Enter Hometown" name="Hometown" id="Hometown" required>
+                        </div>
+                        <div className="personal_details">
+                        <label for="Personal Details"><b>Personal Details</b></label>
+                        </div>
+                        <div className="about_me_q">
+                        <label for="Hometown"><HouseIcon /></label>
+                        <input type="text" placeholder="Enter Hometown" name="pDetail" id="Hometown" required>
                         </input> 
-
-                        <label for="work"><WorkIcon /><b>Work</b></label>
-                        <input type="text" placeholder="Where do you work" name="work" id="work" required>
+                        </div>
+                        <div className="about_me_q">
+                        <label for="work"><WorkIcon /></label>
+                        <input type="text" placeholder="Where do you work" name="pDetail" id="work" required>
                         </input>   
-                        
-                        <label for="education"><SchoolIcon /><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
+                        </div>
+                        <div className="about_me_q">
+                        <label for="education"><SchoolIcon /></label>
+                        <input type="text" placeholder="where do you go to school?" name="pDetail" id="education" required>
                         </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
-                        <label for="education"><b>Education</b></label>
-                        <input type="text" placeholder="where do you go to school?" name="education" id="education" required>
-                        </input>   
+                        </div>  
                         
                         <h1>Extra About Me</h1>
                         <p>Give us the deets...... gender, sexual orientation, height, exercise, smoking drinking zodiac, religion, pets, kids, looking for?, politics</p>
